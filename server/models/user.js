c74385410514daaf37bd27cefd97c3eefaf26c90
2Proton2 <apurva22kalvade@gmail.com>
2023-09-13 20:49:39 +0530

    added userSchema

---
+++ server/models/user.js
@@ -0,0 +1,131 @@
+require('../config/database');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+    type: {
+        type: String,
+        required: true
+    },
+	firstname: {
+        type: String,
+        required: true
+    },
+	middlename: {
+        type: String,
+        required: true
+    },
+	lastname: {
+        type: String,
+        required: true
+    },
+	dob: {
+        type: Date,
+        required: true,
+        validate:{
+            validator: () => {
+                return dob <= new Date();
+            },
+            message: 'Invalid date of birth'
+        }
+    },
+	emailid: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: [true, "%%% Email ID already exist. Please, enter an unique email id %%%"],
+        validate: {
+            validator: function (value) {
+                // Use a regular expression 
+                const mailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if(!(mailRegex.test(value))){
+                    throw new Error("%%% Invalid email %%%");
+                }
+            },
+            message: 'Invalid email address',
+        },
+        message: `Enter a valid email address`
+    },
+	phoneno: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: (value) => {
+                const indianPhNoRegexCase = /^[6-9]\d{9}$/;
+                if(!(indianPhNoRegexCase.test(value))){
+                    throw new Error("%%% Enter a valid Mobile Number %%%")
+                }
+            },
+            message: `Enter a valid Mobile Number`
+        }
+    },
+	shopdetails : {
+		shopname: {
+            type: String,
+            required : true
+        },
+		gstno: {
+            type: String,
+            required: true,
+            validate: {
+                validator : (value) => {
+                    const gstRegexMatchCase = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9A-Z]{1}$/;
+                    if(!(gstRegexMatchCase.test(value))){
+                        throw new Error(`%%% Invalid GST Input %%%`);
+                    }
+                },
+                message: `Invalid GST Input`
+            }
+        }
+	},
+	zipcode: {
+        type: String,
+        required: true
+    },
+	address: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (pass) => {
+                const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
+                if(!(hasSpecialChar.test(pass) && pass.length >= 8)){
+                    throw new Error(`%%% Password requirements are not matched %%%`);
+                }
+            },
+            message: `Password requirements are not fullfilled. Minimum 8 characters long password is needed and atleast one special character`
+        }
+    },
+    cpassword: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (pass) => {
+                const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
+                if(!(hasSpecialChar.test(pass) && pass.length >= 8)){
+                    throw new Error(`%%% Password requirements are not matched %%%`);
+                }
+            },
+            message: `Password requirements are not fullfilled. Minimum 8 characters long password is needed and atleast one special character`
+        }
+    },
+	photo : {
+        type: Array,
+        required: true,
+        validate : {
+            validator: (value) => {
+                if(!(value.length === 4)){
+                    throw new Error(`%%% All required images are uploaded %%%`);
+                }
+            },
+            message: `Please upload all the required images`
+        }
+    }
+})
+
+const user = mongoose.model('user', userSchema);
+module.exports = user;
