8ad27b4775bfea4c4e0fb1851243d42669038ac9
2Proton2 <apurva22kalvade@gmail.com>
2023-09-19 02:10:51 +0530

    error resolved : brought middlewares above routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
 dotenv.config({ path: './config.env' });
 require('./config/database');
 
+/**
+ * I woould like to communicate with server and client in @json format
+ */
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * My endpoints of the website  @routes
@@ -12,11 +19,6 @@ app.use('/cars', require('./routers/cars'));
 app.use('/user', require('./routers/user'));
 app.use('/enquiry', require('./routers/enquiry'));
 app.use('/dealer', require('./routers/dealer'));
-
-/**
- * I woould like to communicate with server and client in @json format
- */
-app.use(express.json());
 
 /**
  * Hey buddy!! I am listening @port 
