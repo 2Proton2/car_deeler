577c488c8a890b32b00eb4e659d19ebe28a82087
2Proton2 <apurva22kalvade@gmail.com>
2023-09-25 00:28:14 +0530

    mailer config generated automatically on ethereal.email website

---
+++ server/config/node-mailer.js
@@ -0,0 +1,12 @@
+const config = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    security: 'STARTTLS',
+    auth: {
+        user: 'orville.littel93@ethereal.email',
+        pass: 'ZthwZCpbCKwXySdVDS'
+    }
+}
+
+
+module.exports = config;
