032cf879559e04594d54f76cff7aa8c5d45f7837
2Proton2 <apurva22kalvade@gmail.com>
2023-10-03 02:43:48 +0530

    added routes in App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Header from './components/Header/index'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home/index';
+import About from './pages/about/index';
+import Contact from './pages/contact/index';
+import Login from './pages/login/index';
+import Signin from './pages/sign/index';
+import Profile from './pages/profile/index';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Header />
+        <Routes>
+          {/* Home */}
+          <Route path='/'>
+            <Route index={true} element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='login' element={<Login />} />
+            <Route path='sign' element={<Signin />} />
+            <Route path='profile' element={<Profile />} />
+          </Route>
+          <Route>
+            <Route />
+          </Route>
+          <Route>
+
+          </Route>
+          {/* About */}
+          {/* Contact */}
+          {/* Login */}
+          {/* car */}
+        </Routes>
     </>
   )
-}
+};
 
-export default App
+export default App;
