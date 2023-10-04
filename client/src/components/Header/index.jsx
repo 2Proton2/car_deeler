a01a1a4eb293688a1aa74227a93c31185974e14a
2Proton2 <apurva22kalvade@gmail.com>
2023-10-05 03:44:28 +0530

    created header with nav bar

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,35 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
-const  Nav = () => {
+import logo from '../../assets/components/Nav/logo/logo.png'
+const Nav = () => {
 
   let isLogin = false;
   return (
     <>
-        <nav>
-            <div className='logo'>
-
-            </div>
-            <div className='search-bar'>
-
-            </div>
-            <div className='nav-pages'>
-              <ul >
-                <li> <NavLink to="/">Home</NavLink> </li>
-                <li> <NavLink to="/about">About Us</NavLink> </li>
-                <li> <NavLink to="/contact">Contact Us</NavLink> </li>
-                {isLogin ? 
-                <>
-                  <li> <NavLink to="/logout">Logout</NavLink> </li>
-                  <li> <NavLink to="/profile">Profile</NavLink> </li>    
-                </> : 
-                <>
-                  <li> <NavLink to="/login">Login</NavLink> </li>
-                </>}
-              </ul>
-              <NavLink />
-            </div>
-        </nav>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center justify-around pt-2 pb-2 pr-20 pl-20">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <img alt="logo" className=" object-cover object-center block" src={logo} />
+            <span className="ml-3 text-xl font-bold text-logoBlue">CAR DEAL</span>
+          </a>
+          <input
+            className="w-1/4 bg-lightBlue text-center text-gray-900 p-1 rounded-lg focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-indigo-400"
+            type="text"
+            placeholder="Search for car dealer ..."
+            required
+          />
+          <nav className="flex flex-wrap items-center text-base justify-center">
+            <NavLink to="/" className="mr-5 hover:cursor-pointer font-bold text-logoBlue">Home</NavLink>
+            <NavLink to="/about" className="mr-5 hover:cursor-pointer font-bold text-logoBlue" >About</NavLink>
+            <NavLink to="/contact" className="mr-5 hover:cursor-pointer font-bold text-logoBlue">Contact</NavLink>
+            <NavLink to="/login" className="mr-5 hover:cursor-pointer font-bold text-logoBlue">Login</NavLink>
+          </nav>
+        </div>
+      </header>
     </>
   )
 }
