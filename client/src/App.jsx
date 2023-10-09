1e0779ace4c278c1859326a9b43f130dc9581928
2Proton2 <apurva22kalvade@gmail.com>
2023-10-10 00:57:13 +0530

    added routes for customer-admin login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Login from './pages/login/index';
 import Signin from './pages/sign/index';
 import Profile from './pages/profile/index';
 import Advertise from './pages/advertise/index';
+import Customer from './pages/login/customer-login/index';
+import Admin from './pages/login/admin-login/index';
 
 const App = () => {
   return (
@@ -19,7 +21,9 @@ const App = () => {
             <Route index={true} element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
-            <Route path='login' element={<Login />} />
+            <Route path='/user/login' element={<Login />} />
+            <Route path='/user/login/admin' element={<Admin/>} />
+            <Route path='/user/login/customer' element={<Customer/>} />
             <Route path='sign' element={<Signin />} />
             <Route path='profile' element={<Profile />} />
             <Route path='advertise' element={<Advertise />}/>
@@ -30,9 +34,6 @@ const App = () => {
           <Route>
 
           </Route>
-          {/* About */}
-          {/* Contact */}
-          {/* Login */}
           {/* car */}
         </Routes>
     </>
