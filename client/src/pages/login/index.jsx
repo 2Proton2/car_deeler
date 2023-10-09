9871b9e693f190e230e51ad1cb5ed57c99476b97
2Proton2 <apurva22kalvade@gmail.com>
2023-10-10 00:56:35 +0530

    ask login prompt page with respective login pages

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import logo from '../../assets/components/Nav/logo/logo.png'
 
 const index = () => {
+  const navigateTo = useNavigate();
+
+  const handleButtonClick = (type) => {
+    navigateTo(`/user/login/${type}`)
+  };
+
   return (
-    <div className='login-page bg-customGray h-screen'>
-      <h2 className='text-center font-bold text-xl'>Login</h2>
-    </div>
+    <section className="bg-customGray">
+      <div className="flex flex-col items-center h-20 justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl text-center mb-10 font-bold leading-tight tracking-tight text-logoBlue md:text-2xl dark:text-white">
+              Login with
+            </h1>
+            <div className="flex flex-row justify-center">
+              <button onClick={() => handleButtonClick('admin')} className="w-full mx-1 text-white bg-red font-medium rounded-lg text-sm px-5 py-2.5 text-center">Admin</button>
+              <button onClick={() => handleButtonClick('customer')} className="w-full mx-1 text-white bg-logoBlue font-medium rounded-lg text-sm px-5 py-2.5 text-center">Customer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
