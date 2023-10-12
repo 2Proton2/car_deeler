c01e36790b5df7dfa34b02f695e25b2b76784dd0
2Proton2 <apurva22kalvade@gmail.com>
2023-10-13 02:44:04 +0530

    resolved cookie bug

--- client/src/service/axios/config/index.js
+++ client/src/service/axios/config/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'http://localhost:4000',
     timeout: 5000,
+    withCredentials: true,
     headers: {
       'Content-Type': 'application/json',
     },
